5d0db0639eb07b6c0da6420add5337c75f87a6f6
Daniel Bennington <dbennington125@gmail.com>
2020-06-29 16:33:55 -0700

    half post routes tests done

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const { agent, getLoggedInUser, prepare } = require('../database/data-helpers');
 const Post = require('../lib/models/Post');
+
 // const User = require('../lib/models/User');
 
 describe('post routes for post model', () => {
@@ -23,6 +24,43 @@ describe('post routes for post model', () => {
           photoUrl: 'whatever@url.com',
           caption: 'this is a caption',
           tags: ['string one', 'string 2', 'rad']
+        });
+      });
+  });
+
+  it('gets all posts', async() => {
+    
+    const posts = prepare(await Post.find());
+    
+    return agent
+      .get('/api/v1/posts')
+      .then(res => {
+        expect(res.body).toEqual(posts);
+      });
+  });
+
+  it('gets post by ID via GET', async() => {
+    
+    const post = prepare(await Post.findOne());
+    
+    return agent
+      .get(`/api/v1/posts/${post._id}`)
+      .then(res => {
+        expect(res.body).toEqual(post);
+      });
+  });
+
+  it('updates caption by ID via PATCH', async() => {
+    const loggedInUser = await getLoggedInUser();
+    const post = prepare(await Post.findOne({ user: loggedInUser._id }));
+    
+    return agent
+      .patch(`/api/v1/posts/${post._id}`)
+      .send({ caption: 'new caption' })
+      .then(res => {
+        expect(res.body).toEqual({
+          ...post,
+          caption:'new caption'
         });
       });
   });

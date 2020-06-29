e0104ec8fd23a7c86e35e21949cc3585d3532b18
Daniel Bennington <dbennington125@gmail.com>
2020-06-29 15:29:38 -0700

    user and auth route tests complete

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -1,7 +1,7 @@
-// const { agent } = 
-require('../database/data-helpers');
+
 const request = require('supertest');
 const app = require('../lib/app');
+const { agent } = require('../database/data-helpers');
 
 describe('auth routes', () => {
   it('signs up a new user with POST', () => {
@@ -17,6 +17,34 @@ describe('auth routes', () => {
           _id: expect.anything(),
           username: 'testusername0',
           profilePhotoUrl: 'imageone.jpg'
+        });
+      });
+  });
+
+  it('can log in a user with POST', () => {
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        username: 'testusername0', 
+        password: 'password1234',
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          username: 'testusername0',
+          profilePhotoUrl: expect.any(String)
+        });
+      });
+  });
+
+  it('can verify if a user is logged in and verified', () => {
+    return agent
+      .get('/api/v1/auth/verify')
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          username: 'testusername0',
+          profilePhotoUrl: expect.any(String)
         });
       });
   });

b8a26575ea24dc2c831a0a986d28f75ff7c957b2
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-06-30 11:40:20 -0700

    trying to merge rachel and my branch

--- __tests__/example.test.js
+++ __tests__/example.test.js
@@ -3,11 +3,6 @@ const mongod = new MongoMemoryServer();
 const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect');
 
-<<<<<<< HEAD:__tests__/example.test
-// const request = require('supertest');
-// const app = require('../lib/app');
-=======
->>>>>>> 254ebccb8bc8ae7fcc036486ea0d4d5a2fb742b9:__tests__/example.test.js
 
 describe('tardygram routes', () => {
   beforeAll(async() => {
